50b0a1859aa43a27641521c08e8898a37e243dfa
justinhlott <justin.h.lott.75@gmail.com>
2026-04-29 11:14:53 -0600

    Rearrange Amy's photos

--- PortraitureByJustinLott/js/jsAmyFamily.js
+++ PortraitureByJustinLott/js/jsAmyFamily.js
@@ -12,11 +12,13 @@ document.addEventListener("DOMContentLoaded",  () => {
 
 const imagesAmy = [
 "back of daughters dress.png",
+"Bride & groom hold picture frame.png",
 "daughter and bride in mommy's shoes.png",
 "daughter holds dad's hand backwards.png",
 "daughter in bride's shoes.png",
 "Ew and kiss.png",
 "family pose.png",
+"Flower girls frame mom and dad kissing.png",
 "hush and kiss.png",
 "pics of kids couple kissing in background.png",
 "Shoes close up kissing couple in background.png"
--- PortraitureByJustinLott/js/jsWeddingPics.js
+++ PortraitureByJustinLott/js/jsWeddingPics.js
@@ -11,19 +11,28 @@ document.addEventListener("DOMContentLoaded",  () => {
 });
 
 const imagesUsed = [
-
 "boquet focus blurry dip.png",
+"boquet toss.jpg",
+"bouquet toss2.jpg",
 "Bride & groom touch foreheads.jpg",
 "Bride behind groom's shoulder.png",
 "Bride holds groom's face.png",
 "Bride in front touching grooms chin.png",
 "closeup couple & rings.png",
+"cut cake 2 strobes.png",
 "Detail invitation.jpg",
 "detail ring1.jpg",
 "detail ring2.jpg",
 "detail ring4.jpg",
 "kissing through ring.png",
 "Nose above lips.jpg",
+"outdoor natural light1.png",
+"outdoor natural light2.png",
+"outdoor natural light3.png",
+"Outdoor strobe.png",
+"shutter drag.png",
 "side view half.png",
-"Side view.png"
+"Side view.png",
+"veil howto1.png"
+
 ];
